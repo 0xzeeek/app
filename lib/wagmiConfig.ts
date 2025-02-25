d9773cd5d7538b4bb603b960393f4e10e1351f88
3agent <threeagent@proton.me>
2025-02-25 16:24:40 -0500

    remove sepolia

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -5,7 +5,7 @@ import { webSocket } from "wagmi";
 const WEBSOCKET_RPC = process.env.NEXT_PUBLIC_WEBSOCKET_RPC_URL;
 
 const config = createConfig({
-  chains: [base, sepolia],
+  chains: [base],
   transports: {
     [base.id]: webSocket(WEBSOCKET_RPC),
     [sepolia.id]: webSocket(WEBSOCKET_RPC),
